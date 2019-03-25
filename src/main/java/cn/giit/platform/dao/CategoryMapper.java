92bb1211cb15e5263d9fae08ab94509ba9f66f49
phw <937855602@qq.com>
2019-03-25 20:03:51 +0800

    增加接口

---
+++ src/main/java/cn/giit/platform/dao/CategoryMapper.java
@@ -0,0 +1,8 @@
+package cn.giit.platform.dao;
+
+import cn.giit.platform.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CategoryMapper extends BaseMapper<Category> {
+}
